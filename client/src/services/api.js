4df669626cb84b7006b20915f351869bfaf26bbc
sharayudesaiehub <sharayudesai96@gmail.com>
2026-07-02 14:40:06 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://jobportal-api-smzv.onrender.com/api",
 });
+
+export default API;
 
 API.interceptors.request.use((config) => {
 
