9b9762727c97e31498ca3995eaa42c544748229e
sharayudesaiehub <sharayudesai96@gmail.com>
2026-07-02 14:53:33 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,18 +4,16 @@ const API = axios.create({
   baseURL: "https://jobportal-api-smzv.onrender.com/api",
 });
 
-export default API;
-
 API.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
     config.headers.Authorization = token;
+    // If your backend expects Bearer tokens, use:
+    // config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
-
 });
 
 export default API;
